a6aa85911728c89aea71b65cc522636586790b14
BaiBing <iBAIBing@outlook.com>
2025-06-16 10:47:49 +0800

    Add demo project which can use ant-javacard

---
+++ src/HelloWorld/PackageInfo.java
@@ -0,0 +1,15 @@
+
+package SCP01Package;
+
+/**
+ * Package Name: SCP01Package
+ *
+ * @version 1.0
+ * @date 25.06.16.10.40.17
+ */
+public final class PackageInfo {
+    public static final byte[] VERSION = new byte[]{(byte)0x1, (byte)0x0,//1.0
+	                                                (byte)0x25, (byte)0x06, (byte)0x16, (byte)0x10, (byte)0x40, (byte)0x17};  //25.06.16.10.40.17
+}
+
+        
